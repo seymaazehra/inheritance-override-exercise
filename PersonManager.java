e8e10524177f88d7f09017ca008e291749e2c116
seymaazehra <75252489+seymaazehra@users.noreply.github.com>
2021-05-11 17:56:30 +0300

    Add files via upload

---
+++ PersonManager.java
@@ -0,0 +1,12 @@
+
+public class PersonManager  {
+	public void List() {
+   	 System.out.println("müþteri listelendi");
+    }
+    
+    public void Add() {
+   	 System.out.println("eklendi");
+   	 
+    }
+    
+}
